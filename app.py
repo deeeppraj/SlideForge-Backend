0967669d7dfce2a4a95e61f1e0b3f9ac4ccc644f
deeeppraj <work.deepraj@gmail.com>
2025-12-18 15:32:43 +0530

    app

--- app.py
+++ app.py
@@ -19,14 +19,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 class UserResponse(BaseModel):
    title:str = Field(...,description="Title of presentation")
